c2ce4a0ffcedda2e7c4b47231102aa6648c2ac3d
Damith Amarasinghe <hello@damithg.dev>
2023-04-20 21:28:26 +0530

    fix: login

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 // Firebase Dev Config
 const firebaseDevConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: "AIzaSyDno4cQJS3tICmjDzP1fR552QIp-JDgCLo",
+  authDomain: "vocal-f8ef5.firebaseapp.com",
+  databaseURL: "https://vocal-f8ef5.firebaseio.com",
+  projectId: "vocal-f8ef5",
+  storageBucket: "vocal-f8ef5.appspot.com",
+  messagingSenderId: 139173415207,
+  appId: "1:139173415207:web:f9fc4b33598d9a6e300102",
+  measurementId: "G-24WGTZ9SCD",
 };
 
 // Exports
